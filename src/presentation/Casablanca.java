bcfefd5a02af4d9890fcfbce529dbdb9062402b4
jakobgaardandersen <jakobgaardandersen@jakobs-mbp.lan>
2014-04-06 17:16:24 +0200

    L&F set to native. Much better look

--- src/presentation/Casablanca.java
+++ src/presentation/Casablanca.java
@@ -3681,7 +3681,7 @@ public class Casablanca extends javax.swing.JFrame
         {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels())
             {
-                if ("Nimbus".equals(info.getName()))
+                if ("Native".equals(info.getName()))
                 {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
